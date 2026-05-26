fe2db6d53cabaf284aba35fc7e46f691ccfc4a73
Aidan Marias <silver0006@kitsunehouse.media>
2026-05-25 23:04:59 -0400

    Added preprocessor for Windows
    - SAL compiles on Windows
    - Needs fallback to conio.h for colorize
    - Need to separate get config path and mk config path. Add optional bool?
    - getconfigpath() should be a wrapper for getconfigpath(std::string). bool would be handled in by both

--- SAL/SAL.cpp
+++ SAL/SAL.cpp
@@ -8,6 +8,9 @@
     #include <termios.h>
     #include <unistd.h>
 #endif
+#if defined(_WIN32) || defined(MSDOS) || defined(__MSDOS__)
+    #include <conio.h>
+#endif
 
 #include <cstdlib>
 #include <filesystem>
@@ -16,9 +19,10 @@
 #include "SAL.hpp"
 
 namespace sal {
+    // std::filesystem was only added in C++17, need fallback, goal c++11
     std::string getConfigPath() {
         std::filesystem::path configPath;
-        #if defined(__unix__) || defined(__APPLE__)
+        #if defined(__linux__) || defined(__unix__) || defined(__APPLE__)
             configPath = std::getenv("XDG_CONFIG_HOME") ? std::getenv("XDG_CONFIG_HOME") : "";
             if (configPath.empty()) {
                 #ifdef DEBUG
@@ -31,12 +35,15 @@ namespace sal {
             }
             std::filesystem::create_directories(configPath);
         #endif
+        #if defined (_WIN32)
+	    configPath = std::getenv("localappdata");
+	#endif
         return configPath.string();
     }
 
     std::string getConfigPath(std::string path) {
         std::filesystem::path configPath;
-        #if defined(__unix__) || defined(__APPLE__)
+        #if defined(__linux__) || defined(__unix__) || defined(__APPLE__)
             configPath = std::getenv("XDG_CONFIG_HOME") ? std::getenv("XDG_CONFIG_HOME") : "";
             if (configPath.empty()) {
                 #ifdef DEBUG
@@ -50,26 +57,36 @@ namespace sal {
             configPath /= path;
             std::filesystem::create_directories(configPath);
         #endif
+	#if defined (_WIN32)
+	    configPath = std::getenv("localappdata");
+	    configPath /= path;
+            std::filesystem::create_directories(configPath);
+	#endif
         return configPath.string();
     }
 
+    // Windows support for ANSI was only added in 10. Need to switch to conio.h
     std::string colorizeString(std::string str, std::string colorId) {
-        #if defined(__unix__) || defined(__APPLE__)
+        #if defined(__linux__) || defined(__unix__) || defined(__APPLE__) || defined(_WIN32)
             return "\e[" + colorId + "m" + str + "\e[0m";
         #endif
 	return str;
     }
 
     char getch() {
-        termios rollback, tmp;
-        char c;
-        tcgetattr(0, &rollback);
-        tmp = rollback;
-        tmp.c_lflag &= ~(ICANON | ECHO);
-        tcsetattr(0, TCSANOW, &tmp);
-        read(0, &c, 1);
-        tcsetattr(0, TCSANOW, &rollback);
-        return c;
+	    char c;
+        #if defined(__unix__) || defined(__APPLE__)
+	    termios rollback, tmp;
+	    tcgetattr(0, &rollback);
+	    tmp = rollback;
+	    tmp.c_lflag &= ~(ICANON | ECHO);
+	    tcsetattr(0, TCSANOW, &tmp);
+	    read(0, &c, 1);
+	    tcsetattr(0, TCSANOW, &rollback);
+	#elif defined(_WIN32)
+	    c = _getch();
+        #endif
+	    return c;
     }
 
 }
